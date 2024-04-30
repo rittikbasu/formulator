bfd1134119707f8d6103a68cf69922f7ffcf28d6
rittikbasu <irittik@gmail.com>
2024-04-30 20:42:20 +0530

    feat: add f1 fonts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,28 @@
 import Image from "next/image";
+import localFont from "next/font/local";
+
+import clsx from "clsx";
+
 import "@/styles/globals.css";
+
+const f1Font = localFont({
+  src: [
+    {
+      path: "../../public/fonts/Formula1-Regular-1.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Formula1-Bold_web.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className="max-w-7xl mx-auto p-8 md:py-16">
+    <div className={clsx("max-w-7xl mx-auto p-8 md:py-16", f1Font.className)}>
       <div className="w-full min-h-screen fixed inset-0 -z-10 bg-black bg-dot-white/[0.2] flex items-center justify-center">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       </div>
