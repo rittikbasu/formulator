9bfd7c2038d238e670ea3adcdcd2c0744946ba6b
rittikbasu <irittik@gmail.com>
2024-06-04 22:55:16 +0530

    feat: add initial page load animation with content sliding in from bottom

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -31,10 +31,12 @@ const f1Font = localFont({
 export default function App({ Component, pageProps }) {
   const [revolve, setRevolve] = useState(true);
   const [isHome, setIsHome] = useState(true);
+  const [showContent, setShowContent] = useState(false);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setRevolve(false);
+      setShowContent(true);
     }, 1000);
 
     return () => clearTimeout(timer);
@@ -43,7 +45,9 @@ export default function App({ Component, pageProps }) {
   const handleImageClick = () => {
     setRevolve(true);
     setIsHome(true);
-    setTimeout(() => setRevolve(false), 1000);
+    setTimeout(() => {
+      setRevolve(false);
+    }, 1000);
   };
 
   const currentYear = new Date().getFullYear();
@@ -56,74 +60,56 @@ export default function App({ Component, pageProps }) {
         <title>Formulator - All Things Formula 1</title>
       </Head>
       <div className={clsx("max-w-7xl mx-auto", f1Font.className)}>
-        <div className="w-full min-h-screen fixed inset-0 -z-10 bg-black bg-dot-white/[0.2] flex items-center justify-center">
+        <div
+          className={clsx(
+            "w-full min-h-screen fixed inset-0 -z-10 bg-black bg-dot-white/[0.2] flex items-center justify-center",
+            "transition-opacity duration-[2000ms]",
+            showContent ? "opacity-100" : "opacity-0"
+          )}
+        >
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         </div>
-        {/* <div className="relative">
-          <div
-            className={clsx(
-              "absolute bottom-0 left-0 right-0 h-px bg-gradient-to-r from-zinc-800 to-zinc-700 lg:mx-2 transition duration-1000",
-              revolve ? "via-fuchsia-600" : "via-red-700"
-            )}
-          ></div>
-          <div className="flex justify-between items-center mb-8 md:mb-0 pt-4 md:pt-6 pb-2 md:pb-4 px-4 bg-black">
-            <Link
-              href={`/teams/${currentYear}`}
-              className="flex justify-center"
-            >
-              <Image
-                src="https://logodownload.org/wp-content/uploads/2016/11/formula-1-logo-7.png"
-                className={clsx(
-                  "transition duration-1000",
-                  revolve && " animate-revolve -hue-rotate-90",
-                  "md:h-[40px] md:w-[160px]"
-                )}
-                onClick={handleImageClick}
-                alt="F1 logo"
-                height={100}
-                width={100}
-                unoptimized={true}
-                priority
-              />
-            </Link>
-            <RaceTimer />
-          </div>
-        </div> */}
-        <NextRaceTicker
-          raceDate={new Date("2024-06-09T00:00:00")}
-          raceName={"Canadian Grand Prix"}
-          circuitName={"Circuit Gilles Villeneuve"}
-        />
-        <Link
-          href={`/teams/${currentYear}`}
-          className="flex justify-center md:my-16 my-8"
+        <div
+          className={clsx(
+            "flex items-center justify-center transition-all ease-in-out duration-1000",
+            showContent ? "min-h-16" : "min-h-screen"
+          )}
         >
-          <Image
-            src="https://logodownload.org/wp-content/uploads/2016/11/formula-1-logo-7.png"
-            className={clsx(
-              "transition duration-1000",
-              revolve && " animate-revolve -hue-rotate-90"
-            )}
-            onClick={handleImageClick}
-            alt="F1 logo"
-            height={100}
-            width={200}
-            unoptimized={true}
-            priority
-          />
-        </Link>
-        <div className="p-8">
-          <Selector isHome={isHome} setIsHome={setIsHome} />
-          <Component {...pageProps} />
-          <footer className="mt-8 md:mt-16">
-            <Link
-              href="https://github.com/rittikbasu/formulator"
-              className="flex items-center justify-center text-zinc-400 hover:text-fuchsia-500 tracking-widest"
-            >
-              source code
-              <FiArrowUpRight className="h-5 w-5 ml-1" />
-            </Link>
-          </footer>
+          <Link
+            href={revolve ? "#" : `/teams/${currentYear}`}
+            className="flex justify-center md:my-16 mt-8 my-12"
+            onClick={!revolve && handleImageClick}
+          >
+            <Image
+              src="https://logodownload.org/wp-content/uploads/2016/11/formula-1-logo-7.png"
+              className={clsx(
+                "transition duration-1000",
+                revolve && " animate-revolve -hue-rotate-90"
+              )}
+              alt="F1 logo"
+              height={100}
+              width={200}
+              unoptimized={true}
+              priority
+            />
+          </Link>
+        </div>
+        <div className={clsx(showContent ? "block" : "hidden")}>
+          <div className="sticky top-0 z-20 w-full">
+            <Selector isHome={isHome} setIsHome={setIsHome} />
+          </div>
+          <div className="p-8">
+            <Component {...pageProps} />
+            <footer className="my-8 md:mt-16">
+              <Link
+                href="https://github.com/rittikbasu/formulator"
+                className="flex items-center justify-center text-zinc-400 hover:text-fuchsia-500 tracking-widest"
+              >
+                source code
+                <FiArrowUpRight className="h-5 w-5 ml-1" />
+              </Link>
+            </footer>
+          </div>
         </div>
       </div>
       <Analytics />
